68734f8685810b200543736024fa2112952cfb72
Jakub Hruby <windstormer4@gmail.com>
2020-04-04 23:55:02 +0200

    Refactoring bunch of things.

--- dominantcolors.go
+++ dominantcolors.go
@@ -1,186 +1,19 @@
 package main
 
 import (
-	"bufio"
-	"encoding/csv"
 	"errors"
-	"fmt"
 	"image"
 	"image/draw"
 	_ "image/gif"
 	_ "image/jpeg"
 	_ "image/png"
-	"io"
 	"math"
-	"net/http"
 	"os"
-	"path"
-	"strconv"
+	"runtime"
 	"sync"
-	"time"
 )
 
-const BUFFER_SIZE = PROC_COUNT
-const RGB_LEN = 3
-
-func DominantColorsFromURLToCSV(urlListFile string, csvFilename string) {
-	linksScanner, fileHandle, err := OpenTheList(urlListFile)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
-	chImgInfo := DownloadAllImages(linksScanner)
-	st := DominantColorsFromRGBAImage(chImgInfo)
-	err = saveEverythingToCSV(st, csvFilename)
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "%v\n", err)
-		fmt.Printf("%v\n", err)
-		os.Exit(1)
-	}
-	fileHandle.Close()
-}
-
-type imageInfo struct {
-	filename string
-	link     string
-	err      error
-}
-
-type processedImage struct {
-	csvInfo [4]string
-	err     error
-}
-
-func DownloadAllImages(linksScanner *bufio.Scanner) <-chan imageInfo {
-	chImgInfo := make(chan imageInfo, BUFFER_SIZE)
-	go func() {
-		for linksScanner.Scan() {
-			url := linksScanner.Text()
-			filename, err := DownloadImage(url)
-			if err != nil {
-				err = fmt.Errorf("failed to download the image: %v", err)
-			}
-			chImgInfo <- imageInfo{filename, url, err}
-		}
-		close(chImgInfo)
-	}()
-	return chImgInfo
-}
-
-func DominantColorsFromRGBAImage(chImgInfo <-chan imageInfo) <-chan processedImage {
-	out := make(chan processedImage, BUFFER_SIZE)
-	go func() {
-		for imgInfo := range chImgInfo { //TODO better goroutines handling
-			o := processedImage{}
-			if imgInfo.err != nil {
-				o.err = imgInfo.err
-				out <- o
-				return
-			}
-			fmt.Println("-- opening the image " + imgInfo.filename)
-			image, Dx, Dy, err := GetRGBAImage(imgInfo.filename)
-			if err != nil {
-				o.err = fmt.Errorf("failed to open the image[%s]: %v", imgInfo.filename, err)
-				out <- o
-				return
-			}
-			fmt.Println("-- opening the image DONE")
-			fmt.Println("-- dominant colors " + imgInfo.filename)
-			colorA, colorB, colorC, err := DominantColors(image, Dx, Dy)
-			if err != nil {
-				o.err = fmt.Errorf("failed to get dominantColors[%s]: %v", imgInfo.filename, err)
-				out <- o
-				return
-			}
-			// os.Remove(imgInfo.filename) //delete image file
-			o.csvInfo = [4]string{imgInfo.link, ColorToRGBHexString(colorA), ColorToRGBHexString(colorB), ColorToRGBHexString(colorC)}
-			out <- o
-		}
-		close(out)
-	}()
-	return out
-}
-
-func saveEverythingToCSV(st <-chan processedImage, csvFilename string) error {
-	//create CSV file,
-	outputCSV, err := os.Create(csvFilename)
-	if err != nil {
-		return fmt.Errorf("failed creating  CSV file: %v", err)
-	}
-	writerCSV := csv.NewWriter(outputCSV)
-	for pi := range st {
-		if pi.err != nil {
-			return pi.err
-		}
-		err = writerCSV.Write(pi.csvInfo[:])
-		if err != nil {
-			return fmt.Errorf("CSV writer failed: %v", err)
-		}
-		fmt.Println("-- adding to CSV[", pi.csvInfo[0], "]")
-	}
-
-	writerCSV.Flush()
-	err = outputCSV.Close()
-	if err != nil {
-		return err
-	}
-	return nil
-}
-
-func OpenTheList(urlListFile string) (*bufio.Scanner, *os.File, error) {
-	file, err := os.Open(urlListFile)
-	if err != nil {
-		return nil, nil, err
-	}
-	scanner := bufio.NewScanner(file)
-	if err := scanner.Err(); err != nil {
-		return nil, nil, err
-	}
-	//we need to return file handle, since we need to close it afterwards
-	return scanner, file, nil
-}
-
-func ColorToRGBHexString(color [RGB_LEN]byte) string {
-	return fmt.Sprintf("#%X%X%X", color[0], color[1], color[2])
-}
-
-func RGBToIntSlice(color []byte) int {
-	r, g, b := int(color[0]), int(color[1]), int(color[2])
-	rgb := ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF)
-	return rgb
-}
-
-func IntToRGB(rgb int) [3]byte {
-	r := (rgb >> 16) & 0xFF
-	g := (rgb >> 8) & 0xFF
-	b := rgb & 0xFF
-	return [3]byte{byte(r), byte(g), byte(b)}
-}
-
-func DownloadImage(url string) (string, error) {
-	fmt.Println("-- downloading " + url)
-	response, err := http.Get(url)
-	if err != nil {
-		return "", err
-	}
-	defer response.Body.Close()
-
-	timePrefix := strconv.FormatInt(int64(time.Now().UnixNano()), 10)
-	filename := timePrefix + "_" + path.Base(url)
-	file, err := os.Create(filename)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	_, err = io.Copy(file, response.Body)
-	if err != nil {
-		return "", err
-	}
-	fmt.Println("-- downloading FINISHED")
-	return filename, nil
-}
+var partitionsCount = runtime.NumCPU()
 
 func GetRGBAImage(imagefilename string) (img *image.RGBA, Dx int, Dy int, err error) {
 	var rgbImage *image.RGBA
@@ -206,10 +39,9 @@ func GetRGBAImage(imagefilename string) (img *image.RGBA, Dx int, Dy int, err er
 	draw.Draw(rgbImage, rgbImage.Bounds(), imageData, imageBounds.Min, draw.Src)
 	return rgbImage, imageBounds.Dx(), imageBounds.Dy(), nil
 }
-
-func DominantColors(image *image.RGBA, width int, height int) ([RGB_LEN]byte, [RGB_LEN]byte, [RGB_LEN]byte, error) {
+func DominantColors(image *image.RGBA, width int, height int) ([RGB_LENGTH]byte, [RGB_LENGTH]byte, [RGB_LENGTH]byte, error) {
 	if width == 0 || height == 0 {
-		var ccA, ccB, ccC [RGB_LEN]byte
+		var ccA, ccB, ccC [RGB_LENGTH]byte
 		return ccA, ccB, ccC, errors.New("image size was 0")
 	}
 	//build a map of unique colors and its sum, pix is array with colors just stacked behind each other
@@ -218,15 +50,19 @@ func DominantColors(image *image.RGBA, width int, height int) ([RGB_LEN]byte, [R
 	const rgbaLen = 4
 
 	lenImgPix := len(imgPix)
-	partitionsCount := PROC_COUNT
 	partitionsLen := int(math.RoundToEven((float64(lenImgPix/rgbaLen) / float64(partitionsCount)))) * rgbaLen
 
+	toIntSlice := func(color []byte) int {
+		r, g, b := int(color[0]), int(color[1]), int(color[2])
+		rgb := ((r & 0xFF) << 16) | ((g & 0xFF) << 8) | (b & 0xFF)
+		return rgb
+	}
 	countPartition := func(imgPix []uint8) <-chan map[int]int {
 		uniqueColorsCh := make(chan map[int]int)
 		go func(imgPix []uint8) {
 			uniqueColors := make(map[int]int)
 			for i := 0; i < len(imgPix); i += rgbaLen {
-				pixel := RGBToIntSlice(imgPix[i : i+4 : i+4])
+				pixel := toIntSlice(imgPix[i : i+4 : i+4])
 				uniqueColors[pixel] = uniqueColors[pixel] + 1
 			}
 			uniqueColorsCh <- uniqueColors
@@ -322,5 +158,13 @@ func DominantColors(image *image.RGBA, width int, height int) ([RGB_LEN]byte, [R
 			rcolors[i] = lastColor
 		}
 	}
-	return IntToRGB(rcolors[0]), IntToRGB(rcolors[1]), IntToRGB(rcolors[2]), nil
+
+	intToRGB := func(rgb int) [3]byte {
+		r := (rgb >> 16) & 0xFF
+		g := (rgb >> 8) & 0xFF
+		b := rgb & 0xFF
+		return [3]byte{byte(r), byte(g), byte(b)}
+	}
+
+	return intToRGB(rcolors[0]), intToRGB(rcolors[1]), intToRGB(rcolors[2]), nil
 }
--- main.go
+++ main.go
@@ -1,16 +1,170 @@
 package main
 
 import (
+	"bufio"
+	"encoding/csv"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"path"
 	"runtime"
+	"strconv"
 	"time"
 )
 
-const PROC_COUNT = 4
+const RGB_LENGTH = 3
+
+var BUFFER_SIZE = runtime.NumCPU()
+
+func DominantColorsFromURLToCSV(urlListFile string, csvFilename string) {
+	openTheList := func(urlListFile string) (*bufio.Scanner, *os.File, error) {
+		file, err := os.Open(urlListFile)
+		if err != nil {
+			return nil, nil, err
+		}
+		scanner := bufio.NewScanner(file)
+		if err := scanner.Err(); err != nil {
+			return nil, nil, err
+		}
+		//we need to return file handle, since we need to close it afterwards
+		return scanner, file, nil
+	}
+
+	linksScanner, fileHandle, err := openTheList(urlListFile)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	chImgInfo := downloadAllImages(linksScanner)
+	st := dominantColorsFromRGBAImage(chImgInfo)
+	err = saveEverythingToCSV(st, csvFilename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "%v\n", err)
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	fileHandle.Close()
+}
+
+type imageInfo struct {
+	filename string
+	link     string
+	err      error
+}
+type processedImage struct {
+	csvInfo [4]string
+	err     error
+}
+
+func downloadAllImages(linksScanner *bufio.Scanner) <-chan imageInfo {
+	chImgInfo := make(chan imageInfo, BUFFER_SIZE)
+
+	downloadImage := func(url string) (string, error) {
+		fmt.Println("-- downloading " + url)
+		response, err := http.Get(url)
+		if err != nil {
+			return "", err
+		}
+		defer response.Body.Close()
+
+		timePrefix := strconv.FormatInt(int64(time.Now().UnixNano()), 10)
+		filename := timePrefix + "_" + path.Base(url)
+		file, err := os.Create(filename)
+		if err != nil {
+			return "", err
+		}
+		defer file.Close()
+
+		_, err = io.Copy(file, response.Body)
+		if err != nil {
+			return "", err
+		}
+		fmt.Println("-- downloading FINISHED")
+		return filename, nil
+	}
+	go func() {
+		for linksScanner.Scan() {
+			url := linksScanner.Text()
+			filename, err := downloadImage(url)
+			if err != nil {
+				err = fmt.Errorf("failed to download the image: %v", err)
+			}
+			chImgInfo <- imageInfo{filename, url, err}
+		}
+		close(chImgInfo)
+	}()
+	return chImgInfo
+}
+
+func dominantColorsFromRGBAImage(chImgInfo <-chan imageInfo) <-chan processedImage {
+	toHexString := func(color [RGB_LENGTH]byte) string {
+		return fmt.Sprintf("#%X%X%X", color[0], color[1], color[2])
+	}
+	out := make(chan processedImage, BUFFER_SIZE)
+	go func() {
+		for imgInfo := range chImgInfo { //TODO better goroutines handling
+			o := processedImage{}
+			if imgInfo.err != nil {
+				o.err = imgInfo.err
+				out <- o
+				return
+			}
+			fmt.Println("-- opening the image " + imgInfo.filename)
+			image, Dx, Dy, err := GetRGBAImage(imgInfo.filename)
+			if err != nil {
+				o.err = fmt.Errorf("failed to open the image[%s]: %v", imgInfo.filename, err)
+				out <- o
+				return
+			}
+			fmt.Println("-- opening the image DONE")
+			fmt.Println("-- dominant colors " + imgInfo.filename)
+
+			colorA, colorB, colorC, err := DominantColors(image, Dx, Dy)
+			if err != nil {
+				o.err = fmt.Errorf("failed to get dominantColors[%s]: %v", imgInfo.filename, err)
+				out <- o
+				return
+			}
+			// os.Remove(imgInfo.filename) //delete image file
+			o.csvInfo = [4]string{imgInfo.link, toHexString(colorA), toHexString(colorB), toHexString(colorC)}
+			out <- o
+		}
+		close(out)
+	}()
+	return out
+}
+
+func saveEverythingToCSV(st <-chan processedImage, csvFilename string) error {
+	//create CSV file,
+	outputCSV, err := os.Create(csvFilename)
+	if err != nil {
+		return fmt.Errorf("failed creating  CSV file: %v", err)
+	}
+	writerCSV := csv.NewWriter(outputCSV)
+	for pi := range st {
+		if pi.err != nil {
+			return pi.err
+		}
+		err = writerCSV.Write(pi.csvInfo[:])
+		if err != nil {
+			return fmt.Errorf("CSV writer failed: %v", err)
+		}
+		fmt.Println("-- adding to CSV[", pi.csvInfo[0], "]")
+	}
+
+	writerCSV.Flush()
+	err = outputCSV.Close()
+	if err != nil {
+		return err
+	}
+	return nil
+}
 
 func main() {
 
-	runtime.GOMAXPROCS(PROC_COUNT) //SET MAX CPUs
+	runtime.GOMAXPROCS(runtime.NumCPU()) //*MAKING SURE WE SET MAX CPUs
 
 	// af := "./testData/test2.jpg"
 	// af = "./testData/bw2.jpg"
@@ -69,5 +223,18 @@ func main() {
 	start := time.Now()
 	DominantColorsFromURLToCSV(testURLFilename, "colors.csv")
 	elapsed := time.Since(start)
+
+	fi, err := os.Stat("colors.csv")
+	if err != nil {
+		fmt.Printf("%v\n", err)
+		os.Exit(1)
+	}
+	// get the size
+	if fi.Size() < 1 {
+		fmt.Fprintf(os.Stderr, "FILE SIZZE CHECK FAILED! ")
+		os.Exit(1)
+	}
+
 	fmt.Printf("Elapsed time: %s", elapsed)
+
 }
